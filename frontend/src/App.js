5cf07a7cc604c4c8edbc9b4c1593c388c7bc12eb
Chiefbark <chiefbark.dev@gmail.com>
2020-09-28 01:26:06 +0200

    Added route for error page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,15 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import Home from './pages/Home';
 import Post from './pages/Post';
+import Error from './pages/404';
 
 export default function App() {
 	return (
 		<BrowserRouter>
 			<Switch>
-				<Route path="/" exact component={Home}/>
-				<Route path="/post/:slug" exact component={Post}/>
+				<Route path={'/'} exact component={Home}/>
+				<Route path={'/post/:slug'} exact component={Post}/>
+				<Route component={Error}/>
 			</Switch>
 		</BrowserRouter>
 	);
