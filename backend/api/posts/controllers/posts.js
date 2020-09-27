018e16904efa12c9401a9b48ca3bb1d9f0cd6fbb
Chiefbark <chiefbark.dev@gmail.com>
2020-09-27 04:06:12 +0200

    Added controller for find & findOne methods

--- backend/api/posts/controllers/posts.js
+++ backend/api/posts/controllers/posts.js
@@ -1,8 +1,28 @@
 'use strict';
 
+const {sanitizeEntity} = require('strapi-utils');
+
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+
+  async find(ctx) {
+    let entity = strapi.services.posts.find({published: true});
+
+    return sanitizeEntity(entity, {model: strapi.models.posts});
+  },
+  async findOne(ctx) {
+    const {id} = ctx.params;
+
+    if (!id) ctx.throw(404, 'Post not found');
+
+    let entity = await strapi.services.posts.find({slug: id, published: true});
+
+    if (entity.length !== 1) ctx.throw(404, 'Post not found');
+
+    return sanitizeEntity(entity, {model: strapi.models.posts});
+  }
+};
