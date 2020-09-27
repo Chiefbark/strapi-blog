56936f7b5d57dbbe807895825fcdbed6c0cfda2c
Chiefbark <chiefbark.dev@gmail.com>
2020-09-27 05:45:01 +0200

    Added PostCard component

---
+++ frontend/src/components/PostCard/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import moment from 'moment';
+
+import './styles.css';
+import {Link} from 'react-router-dom';
+
+const convertDate = (iso_date) => {
+	const date = moment(iso_date);
+	
+	return date.format('MMMM Do YYYY');
+}
+
+export default function PostCard(props) {
+	
+	const {post} = props;
+	
+	return (
+		<article data-slug={post.slug} className={'postCard'}>
+			<img src={`http://localhost:1337${post.thumbnail.url}`} alt={post.slug}/>
+			<div>
+				<h2>{post.title}</h2>
+				<span>{convertDate(post.created_at)}</span>
+				<span>{post.author.username}</span>
+			</div>
+			<Link to={`/posts/${post.slug}`} className={'test'}/>
+		</article>
+	);
+}
