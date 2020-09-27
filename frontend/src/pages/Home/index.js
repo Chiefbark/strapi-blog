e58b6ac5403e5c6d16bc4586f14bbaec082a3e2c
Chiefbark <chiefbark.dev@gmail.com>
2020-09-27 05:45:24 +0200

    Added Home page

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {MdSearch} from 'react-icons/md';
+
+import PostCard from '../../components/PostCard';
+import './styles.css';
+
+const findPosts = async (slug = '') => {
+	const payload = await fetch(`http://localhost:1337/posts/${slug}`);
+	return await payload.json();
+}
 
 export default () => {
+	const [posts, setPosts] = useState([]);
+	const [filtered, setFiltered] = useState([]);
+	const [slug, setSlug] = useState(undefined);
+	const [inputShown, isInputShown] = useState(false);
+	
+	useEffect(() => {
+		findPosts().then(r => {
+			setPosts(r);
+			setFiltered(r);
+		});
+	}, []);
+	
+	useEffect(() => {
+		const list = posts.filter(e => e.slug.includes(slug));
+		setFiltered(list);
+	}, [slug]);
+	
 	return (
 		<div>
-			<h2>HOME PAGE</h2>
+			<nav className={'home-nav'}>
+				<h2>HOME PAGE</h2>
+				<div>
+					<input type={'search'} placeholder={'Type to search posts...'} onChange={value => setSlug(value.target.value)}
+					       className={inputShown && 'shown'}/>
+					<MdSearch size={32} color={inputShown && '#FFF'} onClick={() => isInputShown(true)}/>
+				</div>
+			</nav>
+			<main className={'home-layout'}>
+				{filtered && filtered.map(e => <PostCard key={e.slug} post={e}/>)}
+				{filtered && filtered.map(e => <PostCard key={e.slug} post={e}/>)}
+				{filtered && filtered.map(e => <PostCard key={e.slug} post={e}/>)}
+				{filtered && filtered.map(e => <PostCard key={e.slug} post={e}/>)}
+				{filtered && filtered.map(e => <PostCard key={e.slug} post={e}/>)}
+				{filtered && filtered.map(e => <PostCard key={e.slug} post={e}/>)}
+			</main>
 		</div>
 	)
 }
