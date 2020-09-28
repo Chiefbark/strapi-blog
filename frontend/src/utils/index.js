77350c417a91d1b5eb0bc27d0c38a5cfcc2891ad
Chiefbark <chiefbark.dev@gmail.com>
2020-09-28 23:03:17 +0200

    Added utils for project

---
+++ frontend/src/utils/index.js
@@ -0,0 +1,17 @@
+export const joinParams = (params) => {
+	const entries = Object.entries(params);
+	let query = '?';
+	for (let entry of entries) query += `${entry[0]}=${entry[1]}&`;
+	return query;
+}
+
+export const Constants = {
+	API_URL: 'localhost:1337',
+	END_POINT_POST: 'posts',
+	END_POINT_COUNT: 'count',
+	MAX_LIMIT: 15,
+	QUERY_CONTAINS: '_contains',
+	QUERY_START: '_start',
+	QUERY_LIMIT: '_limit',
+	QUERY_SORT: '_sort'
+}
